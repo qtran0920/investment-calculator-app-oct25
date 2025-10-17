44f8756ce6045d32e02387ab128cec274ce228d4
Jimmy Tran <qtran11@gmu.edu>
2025-10-17 17:40:51 -0400

    feat(investment-results): render investment data in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 import { UserInputComponent } from './user-input/user-input.component';
 
 import type { InvestmentInput } from "./investment-input.model";
@@ -8,10 +9,18 @@ import type { InvestmentInput } from "./investment-input.model";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  resultsData?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment:number,
+    totalInterest: number,
+    totalAmountInvested: number
+  }[];
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
@@ -34,7 +43,7 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 
 }
