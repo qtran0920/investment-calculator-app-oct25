99e0b185ee8cd73ecea7cb7c2790f5f473bc2011
Jimmy Tran <qtran11@gmu.edu>
2025-10-17 17:15:41 -0400

    feat(investment-input): create data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 
+import type { InvestmentInput } from "./investment-input.model";
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,12 +13,7 @@ import { UserInputComponent } from './user-input/user-input.component';
 })
 export class AppComponent {
 
-  onCalculateInvestmentResults(data: {
-    initialInvestment: number,
-    duration: number,
-    expectedReturn: number,
-    annualInvestment: number
-  }) {
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
     const annualData = [];
     let investmentValue = initialInvestment;
