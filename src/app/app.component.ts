922c80592f4aeb34ac2f63d3be0424ca10a3f1e1
Jimmy Tran <qtran11@gmu.edu>
2025-10-31 16:39:17 -0400

    feat(investment-calculator-app-oct25): leverage signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
@@ -13,14 +13,14 @@ import type { InvestmentInput } from "./investment-input.model";
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  resultsData?: {
+  resultsData = signal<{
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment:number,
     totalInterest: number,
     totalAmountInvested: number
-  }[];
+  }[] | undefined >(undefined);
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
@@ -43,7 +43,7 @@ export class AppComponent {
       });
     }
 
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 
 }
